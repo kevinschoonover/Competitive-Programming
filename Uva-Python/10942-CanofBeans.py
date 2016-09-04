648acb484659b8a5c2354f5f43c83cdc36e5d74b
Kevin Schoonover <kevin.m.schoonover@gmail.com>
2016-09-04 12:29:48 -0500

    Started Uva Can of Beans

---
+++ Uva-Python/10942-CanofBeans.py
@@ -0,0 +1,13 @@
+'''
+Programmer:Kevin Schoonover
+Start Date: 2016/09/04
+End Date:
+Program: 10942 - Can of Beans (https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=21&page=show_problem&problem=1883)
+'''
+
+calender = {'January':31,'February':28,'March':31,'April':30,
+            'May':31,'June':30,'July':31,'August':31,'September':30,
+            'October':31,'November':30,'December':31}
+
+def isLeapYear(year):
+    
